f0c66b71416bc9f113c06f561d28c9b414645725
thebigbruv <nonsoogbonna@gmail.com>
2015-08-10 02:56:08 -0400

    Updated python scripts to meet python style guide

--- py/media.py
+++ py/media.py
@@ -1,9 +1,9 @@
 import webbrowser
 
-#Create a Movie class for storing movie related information
+# Create a Movie class for storing movie related information
 class Movie():
     
-    #Define movie properties in the Movie class
+    # Define movie properties in the Movie class
     def __init__(self, title, release_year, rating, storyline, director, stars, poster_image_url, trailer_youtube_url):
         self.title = title
         self.release_year = release_year
@@ -14,6 +14,6 @@ class Movie():
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
     
-    #Define a function in the Movie class that accepts a movie trailer url as input and plays the trailer on a browser    
+    # Define a function in the Movie class that accepts a movie trailer url as input and plays the trailer on a browser    
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
